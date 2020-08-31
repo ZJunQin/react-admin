4749b6d3255136a6f0eb69072290fd6478ac49ed
zjunqin <820317059@qq.com>
2020-08-31 11:00:48 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'font-awesome/css/font-awesome.min.css'
+import './index.scss'
+
+ReactDOM.render(
+    <div>
+        <i className="fa fa-address-book"></i>
+        <h1>Hello, React</h1>
+    </div>,
+    document.getElementById('app')
+)
