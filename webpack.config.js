9bdd9f72599245988c06e42c006afddd1e963b46
zjunqin <820317059@qq.com>
2020-09-09 11:55:00 +0800

    webpack.config file change

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const webpack = require('webpack')
 
+let WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev'
+
 module.exports = {
     entry: './src/app.jsx',
     output: {
